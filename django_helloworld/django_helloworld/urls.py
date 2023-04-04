e0b3688cd876da49483d1453aa2174d33b4cbad5
José Manuel Gilpérez Aguilar <josemanuel.gilperez@uclm.es>
2023-04-04 16:17:26 -0600

    Update

--- django_helloworld/django_helloworld/urls.py
+++ django_helloworld/django_helloworld/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from hello_world.views import index
+from hello_world import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("webfiles/", index)
+    path("", views.index, name = 'index'),
 ]
--- django_helloworld/hello_world/views.py
+++ django_helloworld/hello_world/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return render(request, 'webfiles/index.html')
+  return render(request, "webfiles/index.html")
